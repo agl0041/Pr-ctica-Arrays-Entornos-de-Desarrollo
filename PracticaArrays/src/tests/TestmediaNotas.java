2a616379935f6acb22acc250a5c4c3230bc474d1
agl0041 <agl0041@alu.medac.es>
2024-05-07 16:06:02 +0200

    añadiendo paquete tests y JUnit Test Case TestmediaNotas

---
+++ PracticaArrays/src/tests/TestmediaNotas.java
@@ -0,0 +1,24 @@
+//ALVARO GARCIA LAMUDA
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TestmediaNotas 
+{
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception 
+	{
+		
+	}
+
+	@Test
+	void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
