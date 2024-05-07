ffac49e1e5f82cfb3a62b9a08609d8d5034070ce
agl0041 <agl0041@alu.medac.es>
2024-05-07 16:44:26 +0200

    clase Test para el método mediaNotas

--- PracticaArrays/src/tests/TestmediaNotas.java
+++ PracticaArrays/src/tests/TestmediaNotas.java
@@ -6,19 +6,45 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import util.MisArrays;
+
 class TestmediaNotas 
 {
-
+	static int[] notasValidas; 
+	static int[] notasNoValidas; 
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception 
 	{
+		notasValidas = new int[5];
+		notasValidas[0] = 5;
+		notasValidas[1] = 5;
+		notasValidas[2] = 5;
+		notasValidas[3] = 5;
+		notasValidas[4] = 5;
+		
+		notasNoValidas = new int[5];
+		notasNoValidas[0] = 11;
+		notasNoValidas[1] = 2;
+		notasNoValidas[2] = -3;
+		notasNoValidas[3] = 4;
+		notasNoValidas[4] = 5;
 		
 	}
 
 	@Test
-	void test() 
+    void testMediaNotas_NotasValidas() 
+    {
+        float media = MisArrays.mediaNotas(notasValidas);
+        assertEquals(5, media); 
+    }
+	
+	@Test
+	void testMediaNotas_NotasNoValidas()
 	{
-		fail("Not yet implemented");
+		assertThrows(IllegalArgumentException.class, () -> 
+		{
+			MisArrays.mediaNotas(notasNoValidas); 
+		});
 	}
-
 }
--- PracticaArrays/src/util/MisArrays.java
+++ PracticaArrays/src/util/MisArrays.java
@@ -1,8 +1,6 @@
 //ALVARO GARCIA LAMUDA
 package util;
 
-import java.io.*;
-
 public abstract class MisArrays 
 {
     public static float mediaNotas (int numeros[]) throws IllegalArgumentException
@@ -10,31 +8,18 @@ public abstract class MisArrays
         float media = 0;
         float contador = 0;
         
-        try
+        for (int i = 0; i < numeros.length; i++)
         {
-            for (int i = 0; i < numeros.length; i++)
-            {
-                if (numeros[i] < 0 || numeros[i] > 10)
-                    throw new IllegalArgumentException();
+        	if (numeros[i] < 0 || numeros[i] > 10)
+        		throw new IllegalArgumentException("ERROR: el número tiene que estar entre 0 y 10");
   
-                else
-                {
-                    media += numeros[i];
-                    contador ++;
-                }
-            }  
-        }
+            else
+            {
+            	media += numeros[i];
+                contador ++;
+            }
+        }  
         
-        catch (IllegalArgumentException e)
-        {
-            System.out.println("ERROR: el número tiene que estar entre 0 y 10");
-        }
-        
-        return media/contador;
-				
-    }
-
-    	
-    
-    
+        return (media/contador);			
+    }    
 }
