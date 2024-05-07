91240d25601b40d266d46b2aa9d7026d3d8e8f03
agl0041 <agl0041@alu.medac.es>
2024-05-07 15:53:15 +0200

    método mediaNotas

--- PracticaArrays/src/util/Arrays.java
+++ PracticaArrays/src/util/Arrays.java
@@ -1,5 +1,43 @@
+//ALVARO GARCIA LAMUDA
 package util;
 
-public abstract class Arrays {
+import java.io.*;
 
+public abstract class Arrays 
+{
+    public int mediaNotas (int numeros[]) throws Exception
+    {
+        int media = 0;
+        int contador = 0;
+        
+        try
+        {
+            for (int i = 0; i < numeros.length; i++)
+            {
+                if (numeros[i] < 0 || numeros[i] > 10)
+                {
+                    throw new IOException();
+                }
+                
+                else
+                {
+                    media += numeros[i];
+                    contador ++;
+                }
+
+            }  
+        }
+        
+        catch (IOException e)
+        {
+            System.out.println("ERROR: el número tiene que estar entre 0 y 10");
+        }
+        
+        return media/contador;
+				
+    }
+
+    	
+    
+    
 }
